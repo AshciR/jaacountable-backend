82132737807309b002be8c136420ead3844c8060
Richard Walker <richard.walker.90@gmail.com>
2026-03-20 23:33:36 -0400

    ops: add cannonical logs to api server
    Issues:
    closes #161

--- src/server/app.py
+++ src/server/app.py
@@ -4,8 +4,10 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from config.database import db_config
+from config.log_config import configure_logging
 from src.server.articles.router import router as articles_router
 from src.server.entities.router import router as entities_router
+from src.server.middleware import CanonicalLogMiddleware
 
 API_V1_PREFIX = "/api/v1"
 
@@ -18,7 +20,14 @@ async def lifespan(app: FastAPI):
     await db_config.close_pool()
 
 
+configure_logging()
+
 app = FastAPI(lifespan=lifespan)
+
+# Middlewares
+app.add_middleware(CanonicalLogMiddleware)
+
+# Routers
 app.include_router(articles_router, prefix=API_V1_PREFIX)
 app.include_router(entities_router, prefix=API_V1_PREFIX)
 
